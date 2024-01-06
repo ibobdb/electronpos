e51c129eee5f061a25794b8560f7ea7c8f9f87cb
Boby Nugraha <bobynugraha19@gmail.com>
2024-01-07 06:05:55 +0700

    change env

--- server/config/config.js
+++ server/config/config.js
@@ -8,11 +8,11 @@ module.exports = {
     dialect: 'mysql'
   },
   production: {
-    username: process.env.PRODUCTION_DB_USER || 'root',
-    password: process.env.PRODUCTION_DB_PASSWORD || 'root',
-    database: process.env.PRODUCTION_DB_NAME || 'apotek_development',
-    host: process.env.PRODUCTION_DB_HOST || '127.0.0.1',
-    port: process.env.PRODUCTION_DB_PORT || '3307',
+    username: process.env.PRODUCTION_DB_USER,
+    password: process.env.PRODUCTION_DB_PASSWORD,
+    database: process.env.PRODUCTION_DB_NAME,
+    host: process.env.PRODUCTION_DB_HOST,
+    port: process.env.PRODUCTION_DB_PORT,
     dialect: 'mysql'
   },
   // ...
